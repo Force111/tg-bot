3f15cd9ab47f2459c0cbea9fdbc09a914fe73fe7
Serhiy Bartashevkiy <force@Serhiys-MacBook-Air.local>
2026-06-13 23:11:06 +0300

    testing

---
+++ testleaks.py
@@ -0,0 +1,4 @@
+
+TELEGRAM_KEY = "1234567890:AAFAKE_TEST_TOKEN_DO_NOT_USE_abcdefghijklmnopqrstuvwxyz"
+MONGO_URI = "mongodb+srv://test_user:test_password@test-cluster.mongodb.net/test_db"
+GEMINI_API_KEY = "AIzaSyFAKE_TEST_KEY_DO_NOT_USE_1234567890"
